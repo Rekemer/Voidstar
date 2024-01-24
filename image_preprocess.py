f6386395ded099d42bd8d24d12ce7d18a81a7b85
Ilia <ilya.barselona@gmail.com>
2024-01-24 15:18:02 +0200

    generate pages from image

--- image_preprocess.py
+++ image_preprocess.py
@@ -1,45 +1,78 @@
 from PIL import Image
+import imageio
+import cv2 
+import os
+Image.MAX_IMAGE_PIXELS = None
 #http://www.celestiamotherlode.net/catalog/earth.html JestrEarth JPG 5 levels
 def tx_name(x,y):
     return "tx_" +str(x) +"_"+str(y)+".jpg"
 
-def merge_images(file1, file2):
-    """Merge two images into one, displayed side by side
-    :param file1: path to first image file
-    :param file2: path to second image file
-    :return: the merged Image object
-    """
-    image1 = Image.open(file1)
-    image2 = Image.open(file2)
+def GenerateMipMaps(path,dirname):
+    img = cv2.imread(path)
+    height,width,_ = img.shape
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
+    while width != 0 and height != 0:
+        print("x"+str(width))
+        print("y"+str(height))
+        res = cv2.resize(img, dsize=(int(width), int(height)), interpolation=cv2.INTER_LINEAR)
+        cv2.imwrite( os.path.join(dirname, str(width) +"_"+str(height)+".tiff"), res) 
+        width//=2
+        height//=2
 
-    (width1, height1) = image1.size
-    (width2, height2) = image2.size
+def GeneratePages(page_size_x,page_size_y,mip_maps,max_x,max_y):
+    width, height = max_x,max_y
+    while width >= page_size_x and height >= page_size_y:
+        mipMap = Image.open(mip_maps +"/" +  str(width) +"_"+str(height) +".tiff")
+        page_amount_x = width // page_size_x
+        page_amount_y = height // page_size_y
+        
+        pages_dir=os.path.join(mipMaps , "pages_" +  str(width) +"_"+str(height))
+       # print (pages_dir)
+       # return
+        if not os.path.isdir(pages_dir):
+            os.mkdir(pages_dir)
+        for x in range(page_amount_x):
+            for y in range(page_amount_y):
+                left =page_size_x*x
+                right=page_size_x*x+page_size_x
+                bottom=page_size_y*y+page_size_y
+                top=page_size_y*y
+                subimage = mipMap.crop((left, top, right, bottom))
+                subimage.save(pages_dir + "/"+str(x) +"_"+ str(y) +".png")
 
-    result_width = width1 + width2
-    #result_height = max(height1, height2)
-    result_height = height1+ height2
+        width//=2
+        height//=2
 
-    result = Image.new('RGB', (result_width, result_height))
-    result.paste(im=image1, box=(0, 0))
-    result.paste(im=image2, box=(width1, height1))
-    return result
-tiles_x = 32
-tiles_y = 16
+tiles_x = 64
+tiles_y = 32
 tile_size = (1024,1024)
 virtual_texture_size = (tile_size[0] * tiles_x,tile_size[1] * tiles_y)
-images_path ="JestrEarth JPG/textures/hires/JestrMarble JPG/level4/"
-virtual_image = Image.new('RGB', (virtual_texture_size[0], virtual_texture_size[1]))
+images_path ="JestrEarth JPG/textures/hires/JestrMarble JPG/level5/"
+# virtual_image = Image.new('RGB', (virtual_texture_size[0], virtual_texture_size[1]))
 
-for x in range(tiles_x):
-    for y in range(tiles_y-1):
-        ptr_paste = [tile_size[0]*x,tile_size[1]*y]
-        file_path = images_path + tx_name(x,y)
-        file_path1 = images_path + tx_name(x,y+1)
-        image1 = Image.open(file_path)
-        image2 = Image.open(file_path1)
-        virtual_image.paste(im=image1, box=(ptr_paste[0],ptr_paste[1]))
-        print("x"+str(x))
-        print("y"+str(y))
-        ptr_paste[1] +=tile_size[0]
-        virtual_image.paste(im=image2, box=(ptr_paste[0],ptr_paste[1]))
-virtual_image.save("virtualTex4.tiff", format='TIFF', bigtiff=True)
+# for x in range(tiles_x):
+#     for y in range(tiles_y-1):
+#         ptr_paste = [tile_size[0]*x,tile_size[1]*y]
+#         file_path = images_path + tx_name(x,y)
+#         file_path1 = images_path + tx_name(x,y+1)
+#         image1 = Image.open(file_path)
+#         image2 = Image.open(file_path1)
+#         virtual_image.paste(im=image1, box=(ptr_paste[0],ptr_paste[1]))
+#         print("x"+str(x))
+#         print("y"+str(y))
+#         ptr_paste[1] +=tile_size[0]
+#         virtual_image.paste(im=image2, box=(ptr_paste[0],ptr_paste[1]))
+# imageio.imwrite("virtualTex5.dds", virtual_image)
+#virtual_image.save("virtualTex4.tiff", format='TIFF', bigtiff=True)
+page_size_x  = 128
+page_size_y  = 64
+path = "virtualTex4.tiff"
+virtual_texture= Image.open(path)
+
+width, height = virtual_texture.size
+
+mipMaps = "mipMaps_"+str(path)
+
+GeneratePages(page_size_x,page_size_y,mipMaps,width,height)
+#GenerateMipMaps(path,mipMaps)
